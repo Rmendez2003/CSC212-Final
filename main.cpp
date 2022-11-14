0c35c451f79c19f18d54e33bb495a1143924109f
Donavan Francis <90418030+dfrancis07@users.noreply.github.com>
2022-11-13 20:19:22 -0500

    //Create StartImage and FillVector functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <SFML/Graphics.hpp>
 #include <ctime>
 
 //defines rectangle to hold coordinate information along with height and width 
@@ -15,8 +16,107 @@ struct Rectangle
 	sf::Color colour = sf::Color::White;
 };
 
+//function to fill vector with enough rectangles to fill up the image
+void FillVector(Rectangle rec, std::vector<Rectangle> &rec_vec, sf::Image Image)
+{
+	int xOffset = 0;
+	while (xOffset < Image.getSize().x)
+	{
+        //assign random height for rectangle from 5 to 50 from the top of the window
+		rec.height = rand() % (Image.getSize().y - 50) + 5;
+
+        //assign x offset
+		rec.x_location = xOffset;
+		rec.y_location = Image.getSize().y - rec.height;
+
+		rec_vec.push_back(rec);
+
+		xOffset += 5;
+	}
+}
+
+//Creates the start image by coloring all vectors white in their randomized locations
+void StartImage(sf::Image &image, std::vector<Rectangle> rec_vec)
+{
+
+	for (int i = 0; i < rec_vec.size(); i++)
+	{
+		for (int x = 0; x < rec_vec[i].width; x++)
+		{
+			for (int y = 0; y < rec_vec[i].height; y++)
+			{
+				image.setPixel(rec_vec[i].x_location + x, rec_vec[i].y_location + y, rec_vec[i].colour);
+			}
+		}
+	}
+	
+}
+
+
+
+//Sorts the rectangles using the bubblesort method
+void BubbleSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite)
+{
+	Rectangle tempRec;
+	int currX;
+
+	for(int i =0; i < rec_vec.size(); i++)
+	{
+		for (int j = 0; j < rec_vec.size() -1; j++)
+		{   
+            //if value to the right is less than the current postion value
+			if(rec_vec[j].height > rec_vec[j + 1].height)
+			{
+				window.clear();
+
+				//Draw in bigger rectangle, at the x position of the smaller rectangle
+				for (int x = 0; x < rec_vec[j].width; x++)
+				{
+					for (int y = 0; y < rec_vec[j].height; y++)
+					{
+						image.setPixel(rec_vec[j + 1].x_location + x, rec_vec[j].y_location + y, rec_vec[j].colour);
+					}
+				}
+				//delete bigger rectangle by setting all of its pixels to black
+				for (int x = 0; x < rec_vec[j].width; x++)
+				{
+					for (int y = 0; y < rec_vec[j].height; y++)
+					{
+						image.setPixel(rec_vec[j].x_location + x, rec_vec[j].y_location + y, sf::Color::Black);
+					}
+				}
+				//draws smaller rectangle in postion of the older rectangle
+				for (int x = 0; x < rec_vec[j + 1].width; x++)
+				{
+					for (int y = 0; y < rec_vec[j + 1].height; y++)
+					{
+						image.setPixel(rec_vec[j].x_location + x, rec_vec[j + 1].y_location + y, rec_vec[j].colour);
+					}
+				}
+
+                //swaps x locations of the two rectangles
+				currX = rec_vec[j].x_location;
+				rec_vec[j].x_location = rec_vec[j + 1].x_location;
+				rec_vec[j + 1].x_location = currX;
+				
+                //swaps big and small rectangles' positions in the vector
+				tempRec = rec_vec[j];
+				rec_vec[j] = rec_vec[j + 1];
+				rec_vec[j + 1] = tempRec;
+
+                //updates image and draws in on the window
+				texture.loadFromImage(image);
+				sprite.setTexture(texture);
+				window.draw(sprite);
+				window.display();
+			}
+		}
+	}
+}
+
 int main()
 {
+    
 	srand(time(NULL));
 
     //opens window and creates event
@@ -24,7 +124,7 @@ int main()
 	int height = 800;
 	bool spaceIspressed = false;
 
-	sf::RenderWindow window(sf::VideoMode(width, height), "Bubble Sort");
+	sf::RenderWindow window(sf::VideoMode(width, height), "Merge Sort");
 	sf::Event ev;
 
     //creates an image to be diplayed on the window

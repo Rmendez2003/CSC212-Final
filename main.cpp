842de33fe8e1513c1390bf68039b53c1ae34a43b
Caleb Cyr <90429575+cyrcaleb@users.noreply.github.com>
2022-12-04 14:18:49 -0500

    updated to that now we are using the account class

--- main.cpp
+++ main.cpp
@@ -1,41 +1,35 @@
+#include "account.hpp"
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <set>
 
-//defines rectangle to hold coordinate information along with height and width 
-struct Rectangle
-{
-	int height = 0;
-	int width = 3;
-
-	int x_location = 0;
-	int y_location = 0;
-
-	sf::Color colour = sf::Color::White;
-};
-
-void printArray(std::vector<Rectangle> vect){
-    for(int i = 0; i < vect.size(); i++){
-        std::cout << vect[i].height << ", ";
-    }
-    std::cout << std::endl;
+//function to blackout a rectangle
+void Blackout(std::vector<Account> &rec_vec, sf::Image &image, int Idx){
+	for(int i = 0; i < 325; i++){
+		for (int x = 0; x < rec_vec[Idx].width; x++)
+		{
+			for (int y = 0; y < rec_vec[Idx].balance; y++)
+			{
+				image.setPixel(rec_vec[Idx].x_location + x, rec_vec[Idx].y_location + y, sf::Color::Black);
+			}
+		}
+	}
 }
 
-
-
 //function to fill vector with enough rectangles to fill up the image
-void FillVector(Rectangle rec, std::vector<Rectangle> &rec_vec, sf::Image Image)
+void FillVector(Account rec, std::vector<Account> &rec_vec, sf::Image Image)
 {
 	int xOffset = 0;
-	while (xOffset < Image.getSize().x)
+	while (xOffset < Image.getSize().x - 35)  // 5 times 4+3 (offset + width) so we can add more accounts 
 	{
-        //assign random height for rectangle from 5 to 50 from the top of the window
-		rec.height = rand() % (Image.getSize().y - 50) + 5;
+        //assign random balance for rectangle from 5 to 50 from the top of the window
+		rec.balance = rand() % (Image.getSize().y - 50) + 5;
 
         //assign x offset
 		rec.x_location = xOffset;
-		rec.y_location = Image.getSize().y - rec.height;
+		rec.y_location = Image.getSize().y - rec.balance;
 
 		rec_vec.push_back(rec);
 
@@ -44,14 +38,14 @@ void FillVector(Rectangle rec, std::vector<Rectangle> &rec_vec, sf::Image Image)
 }
 
 //Creates the start image by coloring all vectors white in their randomized locations
-void StartImage(sf::Image &image, std::vector<Rectangle> rec_vec)
+void StartImage(sf::Image &image, std::vector<Account> rec_vec)
 {
 	
 	for (int i = 0; i < rec_vec.size(); i++)
 	{
 		for (int x = 0; x < rec_vec[i].width; x++)
 		{
-			for (int y = 0; y < rec_vec[i].height; y++)
+			for (int y = 0; y < rec_vec[i].balance; y++)
 			{
 				image.setPixel(rec_vec[i].x_location + x, rec_vec[i].y_location + y, rec_vec[i].colour);
 			}
@@ -61,13 +55,13 @@ void StartImage(sf::Image &image, std::vector<Rectangle> rec_vec)
 }
 
 //function to swap the position of two vectors
-void swapPos(int bigIdx, int smallIdx, std::vector<Rectangle>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite){
+void swapPos(int bigIdx, int smallIdx, std::vector<Account>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int &xOffset){
 	window.clear();
 
 	//Draw in bigger rectangle, at the x position of the smaller rectangle
 	for (int x = 0; x < rec_vec[bigIdx].width; x++)
 	{
-		for (int y = 0; y < rec_vec[bigIdx].height; y++)
+		for (int y = 0; y < rec_vec[bigIdx].balance; y++)
 		{
 			image.setPixel(rec_vec[smallIdx].x_location + x, rec_vec[bigIdx].y_location + y, rec_vec[bigIdx].colour);
 		}
@@ -75,7 +69,7 @@ void swapPos(int bigIdx, int smallIdx, std::vector<Rectangle>& rec_vec, sf::Imag
 	//delete bigger rectangle by setting all of its pixels to black
 	for (int x = 0; x < rec_vec[bigIdx].width; x++)
 	{
-		for (int y = 0; y < rec_vec[bigIdx].height; y++)
+		for (int y = 0; y < rec_vec[bigIdx].balance; y++)
 		{
 			image.setPixel(rec_vec[bigIdx].x_location + x, rec_vec[bigIdx].y_location + y, sf::Color::Black);
 		}
@@ -84,13 +78,15 @@ void swapPos(int bigIdx, int smallIdx, std::vector<Rectangle>& rec_vec, sf::Imag
 	//draws smaller rectangle in postion of the older rectangle
 	for (int x = 0; x < rec_vec[smallIdx].width; x++)
 	{
-		for (int y = 0; y < rec_vec[smallIdx].height; y++)
+		for (int y = 0; y < rec_vec[smallIdx].balance; y++)
 		{
-			image.setPixel(rec_vec[bigIdx].x_location + x, rec_vec[smallIdx].y_location + y, rec_vec[bigIdx].colour);
+			image.setPixel(rec_vec[bigIdx].x_location + x, rec_vec[smallIdx].y_location + y, rec_vec[smallIdx].colour);
 		}
 	}
-	Rectangle tempRec;
+	Account tempRec(image, xOffset);
 	int currX;
+	sf::Color col;
+
 
 	//swaps x locations of the two rectangles
 	currX = rec_vec[bigIdx].x_location;
@@ -110,17 +106,17 @@ void swapPos(int bigIdx, int smallIdx, std::vector<Rectangle>& rec_vec, sf::Imag
 }
 
 //function to draw the current orientation of the vector on screen
-void DrawVec(std::vector<Rectangle>& printVector, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int width, int height){
+void DrawVec(std::vector<Account>& printVector, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int width, int balance){
 	window.clear();
 
 	for (int i = 0; i < printVector.size(); i++)
 	{	
-		//fills in the current vector in white
+		//fills in the current vector in
 		for (int x = 0; x < printVector[i].width; x++)
 		{
-			for (int y = 0; y < printVector[i].height; y++)
+			for (int y = 0; y < printVector[i].balance; y++)
 			{
-				image.setPixel(printVector[i].x_location + x, printVector[i].y_location + y, sf::Color::White);
+				image.setPixel(printVector[i].x_location + x, printVector[i].y_location + y, printVector[i].colour);
 			}
 		}
 	}
@@ -132,8 +128,25 @@ void DrawVec(std::vector<Rectangle>& printVector, sf::Image& image, sf::RenderWi
 	window.display();
 }
 
+//helper function for quicksort that adjusts the vector in place based around the pivot which we have made the first index
+int partition(std::vector<Account>& rec_vec, int start, int end, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int &xOffset)
+{
+
+    Account pivot = rec_vec[end];
+    int pivotIndex = 0;
+    for (int i = 0; i < end; i++) {
+        if(rec_vec[i].balance <= pivot.balance){  
+			swapPos(pivotIndex, i, rec_vec, image, window, texture, sprite, xOffset);
+			pivotIndex++;
+        }
+    }
+	swapPos(pivotIndex, end, rec_vec, image, window, texture, sprite, xOffset);
+
+    return pivotIndex;
+}
+
 //Sorts the rectangles using the quick sort method
-void QuickSort(std::vector<Rectangle>& rec_vec, int start, int end, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite)
+void QuickSort(std::vector<Account>& rec_vec, int start, int end, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int &xOffset)
 {
 
     // base case
@@ -141,43 +154,43 @@ void QuickSort(std::vector<Rectangle>& rec_vec, int start, int end, sf::Image& i
         return;
 
     // partitioning the array
-    int p = partition(rec_vec, start, end, image, window, texture, sprite);
+    int p = partition(rec_vec, start, end, image, window, texture, sprite, xOffset);
 
     // Sorting the left part
-    QuickSort(rec_vec, start, p - 1, image, window, texture, sprite);
+    QuickSort(rec_vec, start, p - 1, image, window, texture, sprite, xOffset);
 
     // Sorting the right part
-    QuickSort(rec_vec, p + 1, end, image, window, texture, sprite);
+    QuickSort(rec_vec, p + 1, end, image, window, texture, sprite, xOffset);
 }
 
 //Sorts the rectangles using the merge sort method
-void MergeSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int width, int height){
+void MergeSort(std::vector<Account>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int width, int balance){
     if(rec_vec.size() < 2){
         return;
     }
 	//get mid point and segment vector into left and right vector
     int mid = rec_vec.size()/2;
 
-    std::vector<Rectangle> left;
+    std::vector<Account> left;
     for(int i = 0; i < mid; i++){
         left.push_back(rec_vec[i]);
     }
 
 
-    std::vector<Rectangle> right;
+    std::vector<Account> right;
     for(int i = mid; i < rec_vec.size(); i++){
         right.push_back(rec_vec[i]);
     }
 
 
 	//recursively call function on segmented vectors
-    MergeSort(left, image, window, texture, sprite, width, height);
-    MergeSort(right, image, window, texture, sprite, width, height);
+    MergeSort(left, image, window, texture, sprite, width, balance);
+    MergeSort(right, image, window, texture, sprite, width, balance);
 
     //combine vectors
     int leftIdx = 0, rightIdx = 0, currIdx = 0;
     while(leftIdx < left.size() && rightIdx < right.size()){
-		if(left[leftIdx].height < right[rightIdx].height){
+		if(left[leftIdx].balance < right[rightIdx].balance){
 			Blackout(left, image, leftIdx);
 			//change the original vector at the current index with the left vector at the left index
 			left[leftIdx].x_location = rec_vec[currIdx].x_location;
@@ -212,11 +225,11 @@ void MergeSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::RenderWind
     }
 
 	//drawn multiple times to visulaize easier
-	DrawVec(rec_vec, image, window, texture, sprite, width, height);
+	DrawVec(rec_vec, image, window, texture, sprite, width, balance);
 }
 
 //Sorts the rectangles using the insertionsort method  (Selection sort appears extremly fast since only a max of N swaps need to occur unlike in insertion sort and in bubble sort)
-void SelectionSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite)
+void SelectionSort(std::vector<Account>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int &xOffset)
 {
     int i, j, min_idx;
  
@@ -231,9 +244,9 @@ void SelectionSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::Render
 			//slow down rate due to swapping
 			for (int x = 0; x < rec_vec[j].width; x++)
 			{
-				for (int y = 0; y < rec_vec[j].height; y++)
+				for (int y = 0; y < rec_vec[j].balance; y++)
 				{
-					image.setPixel(rec_vec[j].x_location + x, rec_vec[j].y_location + y, sf::Color::White);
+					image.setPixel(rec_vec[j].x_location + x, rec_vec[j].y_location + y, rec_vec[j].colour);
 				}
 			}
 			//updates image and draws in on the window
@@ -242,22 +255,22 @@ void SelectionSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::Render
 			window.draw(sprite);
 			window.display();
 
-			if (rec_vec[j].height < rec_vec[min_idx].height){
+			if (rec_vec[j].balance < rec_vec[min_idx].balance){
 				min_idx = j;
 			}
 		}
 		
         //swap minimum element with current boundary
         if(min_idx != i){
-			swapPos(i, min_idx, rec_vec, image, window, texture, sprite);
+			swapPos(i, min_idx, rec_vec, image, window, texture, sprite, xOffset);
 		}
     }
 }
 
 //Sorts the rectangles using the insertionsort method
-void InsertionSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite)
+void InsertionSort(std::vector<Account>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int xOffset)
 {
-    Rectangle tempRec;
+    Account tempRec(image, xOffset);
 	int j;
 	
 	//for loop through each element
@@ -266,10 +279,10 @@ void InsertionSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::Render
         tempRec = rec_vec[i];
         j = i - 1;
 
-        while (j >= 0 && rec_vec[j].height > tempRec.height)
+        while (j >= 0 && rec_vec[j].balance > tempRec.balance)
         {
             //rec_vec[j + 1] = rec_vec[j];
-			swapPos(j, j+1, rec_vec, image, window, texture, sprite);
+			swapPos(j, j+1, rec_vec, image, window, texture, sprite, xOffset);
             j--;
         }
     }
@@ -277,16 +290,16 @@ void InsertionSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::Render
 
 
 //Sorts the rectangles using the bubblesort method
-void BubbleSort(std::vector<Rectangle>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite)
+void BubbleSort(std::vector<Account>& rec_vec, sf::Image& image, sf::RenderWindow &window, sf::Texture texture, sf::Sprite sprite, int xOffset)
 {
 	for(int i = 0; i < rec_vec.size(); i++)
 	{
 		for (int j = 0; j < rec_vec.size() -1; j++)
 		{   
             //if value to the right is less than the current postion value
-			if(rec_vec[j].height > rec_vec[j + 1].height)
+			if(rec_vec[j].balance > rec_vec[j + 1].balance)
 			{
-				swapPos(j, j+1, rec_vec, image, window, texture, sprite);
+				swapPos(j, j+1, rec_vec, image, window, texture, sprite, xOffset);
 			}
 		}
 	}
